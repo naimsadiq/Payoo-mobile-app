be829449d4ebf622e6aade302f0886307e4456c4
naimsadiq <nayemsadiq2013@gmail.com>
2025-08-26 15:10:32 +0600

    finel project complete

--- home.js
+++ home.js
@@ -25,6 +25,24 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
     const addAmount = parseInt(document.getElementById('add-amount').value);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText) + addAmount;
     document.getElementById('available-balance').innerText = availableBalance;
+
+    // transction add 
+    const addMoneyBank = document.getElementById('add-money-bank').value;
+    let timeShow = new Date().toLocaleTimeString();
+    const cardConteiner = document.getElementById('card-conteiner');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div id="card" class="bg-[#FFFFFF] flex justify-between items-center p-3.5 rounded-[10px] mb-3.5">
+                                <div class="flex items-center">
+                                    <div class="w-[50px] h-[50px] rounded-3xl bg-[#0808080d] flex items-center justify-center "><img src="./assets/wallet1.png" alt=""></div>
+                                        <div class="ml-[15px]">
+                                            <h4 class="text-[#080808b3] font-semibold">${addMoneyBank}</h4>
+                                            <p class="text-[12px] text-[#080808b3]">Today <span>${timeShow}</span></p>
+                                        </div>
+                                    </div>
+                                <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                            </div>` 
+    
+    cardConteiner.appendChild(newElement);
 })
 
 // cash out feature 
@@ -39,6 +57,25 @@ document.getElementById('withdraw-btn').addEventListener('click', function(event
     const cashOutValue = parseInt(document.getElementById('cash-out-amount').value);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText) - cashOutValue;
     document.getElementById('available-balance').innerText = availableBalance;
+    const bkash = 'Mobile Recharge';
+
+    // transction add 
+    // const addMoneyBank = document.getElementById('add-money-bank').value;
+    let timeShow = new Date().toLocaleTimeString();
+    const cardConteiner = document.getElementById('card-conteiner');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div id="card" class="bg-[#FFFFFF] flex justify-between items-center p-3.5 rounded-[10px] mb-3.5">
+                                <div class="flex items-center">
+                                    <div class="w-[50px] h-[50px] rounded-3xl bg-[#0808080d] flex items-center justify-center "><img src="./assets/wallet1.png" alt=""></div>
+                                        <div class="ml-[15px]">
+                                            <h4 class="text-[#080808b3] font-semibold">${bkash}</h4>
+                                            <p class="text-[12px] text-[#080808b3]">Today <span>${timeShow}</span></p>
+                                        </div>
+                                    </div>
+                                <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                            </div>` 
+    
+    cardConteiner.appendChild(newElement);
 })
 
 
@@ -54,6 +91,25 @@ document.getElementById('send-now-btn').addEventListener('click', function(event
     const transferMoneyValue = parseInt(document.getElementById('transfer-money-amount').value);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText) - transferMoneyValue;
     document.getElementById('available-balance').innerText = availableBalance;
+    const diposit = 'DipoBank Deposit';
+
+    // transction add 
+    // const addMoneyBank = document.getElementById('add-money-bank').value;
+    let timeShow = new Date().toLocaleTimeString();
+    const cardConteiner = document.getElementById('card-conteiner');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div id="card" class="bg-[#FFFFFF] flex justify-between items-center p-3.5 rounded-[10px] mb-3.5">
+                                <div class="flex items-center">
+                                    <div class="w-[50px] h-[50px] rounded-3xl bg-[#0808080d] flex items-center justify-center "><img src="./assets/wallet1.png" alt=""></div>
+                                        <div class="ml-[15px]">
+                                            <h4 class="text-[#080808b3] font-semibold">${diposit}</h4>
+                                            <p class="text-[12px] text-[#080808b3]">Today <span>${timeShow}</span></p>
+                                        </div>
+                                    </div>
+                                <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                            </div>` 
+    
+    cardConteiner.appendChild(newElement);
 })
 
 
@@ -69,6 +125,43 @@ document.getElementById('get-bonus-btn').addEventListener('click', function(even
     document.getElementById('available-balance').innerText = availableBalance;
     console.log(typeof(availableBalance));
 })
+
+// Pay Bill feature 
+document.getElementById('pay-bill-btn').addEventListener('click', function (event){
+    event.preventDefault();
+    const payBillAccountNumber = document.getElementById('pay-bill-account-number').value;
+    const payBillPinNumber = parseInt(document.getElementById('pay-bill-pin-number').value);
+    if(payBillAccountNumber.length !== accountNumberDigit || payBillPinNumber !== defultPinNumber){
+        alert('Invalid Information');
+        return; 
+    }
+    const payBillAmount = parseInt(document.getElementById('pay-bill-amount').value);
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText) - payBillAmount;
+    document.getElementById('available-balance').innerText = availableBalance;
+    // alert('Bill Submit');
+
+
+    // transction add 
+    const addMoneyBank = document.getElementById('pay-bill-bank').value;
+    let timeShow = new Date().toLocaleTimeString();
+    const cardConteiner = document.getElementById('card-conteiner');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div id="card" class="bg-[#FFFFFF] flex justify-between items-center p-3.5 rounded-[10px] mb-3.5">
+                                <div class="flex items-center">
+                                    <div class="w-[50px] h-[50px] rounded-3xl bg-[#0808080d] flex items-center justify-center "><img src="./assets/wallet1.png" alt=""></div>
+                                        <div class="ml-[15px]">
+                                            <h4 class="text-[#080808b3] font-semibold">${addMoneyBank}</h4>
+                                            <p class="text-[12px] text-[#080808b3]">Today <span>${timeShow}</span></p>
+                                        </div>
+                                    </div>
+                                <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                            </div>` 
+    
+    cardConteiner.appendChild(newElement);
+})
+
+// transactions feature 
+
 
 // toggle feature 
 
@@ -91,4 +184,7 @@ document.getElementById('get-bonus').addEventListener('click', function(){
 })
 document.getElementById('pay-bill').addEventListener('click', function(){
     formShow('pay-bill-section')
+})
+document.getElementById('transactions').addEventListener('click', function(){
+    formShow('transaction-history-section')
 })
